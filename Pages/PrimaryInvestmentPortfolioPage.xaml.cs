2016fa56eb12f4f74a848fcaaa317e0677773681
USER <USER@DESKTOP-8LRHPT1>
2024-02-21 12:09:18 +0400

    Вынесено определение стратегии в отдельный метод

--- Pages/PrimaryInvestmentPortfolioPage.xaml.cs
+++ Pages/PrimaryInvestmentPortfolioPage.xaml.cs
@@ -20,9 +20,10 @@ namespace InvestmentAssistant.Pages
     {
         /// <summary> Экземпляр класса для заполнения данными combobox</summary>
         StrategyAndConditions strategyAndConditions = new StrategyAndConditions();
+        /// <summary> Экземпляр класса для стратегий</summary>
+        StrategyService strategyService = new StrategyService();
 
         string[] userSelection = { "null", "null", "null", "null" };
-        //int[,] matches = new int[5,4];
 
         public PrimaryInvestmentPortfolioPage()
         {
@@ -49,75 +50,25 @@ namespace InvestmentAssistant.Pages
         private void investmentGoalComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             userSelection[0] = investmentGoalComboBox.SelectedItem.ToString();
-            strategyTextBlock.Text = DefinitionOfStrategy();
+            strategyTextBlock.Text = strategyService.DefinitionOfStrategy(investmentGoalComboBox, investmentHorizonComboBox, riskAccountingComboBox, expectedReturnComboBox, strategyAndConditions, userSelection);
         }
 
         private void investmentHorizonComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             userSelection[1] = investmentHorizonComboBox.SelectedItem.ToString();
-            strategyTextBlock.Text = DefinitionOfStrategy();
+            strategyTextBlock.Text = strategyService.DefinitionOfStrategy(investmentGoalComboBox, investmentHorizonComboBox, riskAccountingComboBox, expectedReturnComboBox, strategyAndConditions, userSelection);
         }
 
         private void riskAccountingComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             userSelection[2] = riskAccountingComboBox.SelectedItem.ToString();
-            strategyTextBlock.Text = DefinitionOfStrategy();
+            strategyTextBlock.Text = strategyService.DefinitionOfStrategy(investmentGoalComboBox, investmentHorizonComboBox, riskAccountingComboBox, expectedReturnComboBox, strategyAndConditions, userSelection);
         }
 
         private void expectedReturnComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             userSelection[3] = expectedReturnComboBox.SelectedItem.ToString();
-            strategyTextBlock.Text = DefinitionOfStrategy();
-        }
-
-        /// <summary> Метод выбора стратегии (потом куда нибудь выкину) </summary>
-        public string DefinitionOfStrategy()
-        {
-            string result;
-            if (investmentGoalComboBox.SelectedItem == null || investmentHorizonComboBox.SelectedItem == null || riskAccountingComboBox.SelectedItem == null || expectedReturnComboBox.SelectedItem == null )
-            {
-                return result ="Заполниет все поля" ;
-            }
-            int[,] matches = new int[strategyAndConditions.StrategyAndConditionsList.Count, userSelection.Length];
-
-            for (int i = 0; i < strategyAndConditions.StrategyAndConditionsList.Count; i++)
-            {
-                Strategy strategy = strategyAndConditions.StrategyAndConditionsList[i];
-                matches[i, 0] = strategy.InvestmentGoal == userSelection[0] ? 1 : 0;
-                matches[i, 1] = strategy.InvestmentHorizon == userSelection[1] ? 1 : 0;
-                matches[i, 2] = strategy.RiskAccounting == userSelection[2] ? 1 : 0;
-                matches[i, 3] = strategy.ExpectedReturn == userSelection[3] ? 1 : 0;
-            }
-
-            int maxMatches = 0;
-            int bestMatchIndex = -1;
-
-            for (int i = 0; i < strategyAndConditions.StrategyAndConditionsList.Count; i++)
-            {
-                int currentMatches = 0;
-                for (int j = 0; j < userSelection.Length; j++)
-                {
-                    currentMatches += matches[i, j];
-                }
-
-                if (currentMatches > maxMatches)
-                {
-                    maxMatches = currentMatches;
-                    bestMatchIndex = i;
-                }
-            }
-           
-            if (bestMatchIndex != -1)
-            {
-                Strategy bestMatchedStrategy = strategyAndConditions.StrategyAndConditionsList[bestMatchIndex];
-                result = bestMatchedStrategy.Name;
-            }
-            else
-            {
-                result = "Подходящая стратегия не найдена";
-            }
-
-            return result;
-        }
+            strategyTextBlock.Text = strategyService.DefinitionOfStrategy(investmentGoalComboBox, investmentHorizonComboBox, riskAccountingComboBox, expectedReturnComboBox, strategyAndConditions, userSelection);
+        }       
     }
 }

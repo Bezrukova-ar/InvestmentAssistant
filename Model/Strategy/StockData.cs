1317a804588b2178af6be161394c77d4efa69872
USER <USER@DESKTOP-8LRHPT1>
2024-02-21 14:16:53 +0400

    Модель данных для расчетов по выбранной стратегии

---
+++ Model/Strategy/StockData.cs
@@ -0,0 +1,15 @@
+﻿namespace InvestmentAssistant.Model.Strategy
+{
+    /// <summary> Представляет данные о ценных бумагах</summary>
+    class StockData
+    {
+        /// <summary> Уникальный код ценной бумаги </summary>
+        public string SecurityId { get; set; }
+        /// <summary> Название ценной бумаги </summary>
+        public string SecurityName { get; set; }
+        /// <summary> Прогнозируемая доходность акции </summary>
+        public string ProjectedStockReturn { get; set; }
+        /// <summary> Риск акции </summary>
+        public string StockRisk { get; set; }
+    }
+}
